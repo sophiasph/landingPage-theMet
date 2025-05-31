5eb954c8fe0067e4081c54e2d939e73451ee9868
Sophia <sophia13gal@gmail.com>
2025-05-31 16:13:06 +0300

    make build

---
+++ landingPage-theMet.1112d100.js
@@ -0,0 +1,2 @@
+"use strict";var form=document.getElementById("myForm");form.addEventListener("submit",function(e){e.preventDefault(),form.reset()});
+//# sourceMappingURL=landingPage-theMet.1112d100.js.map
